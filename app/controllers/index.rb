184c7b9514a88bb7bb70f25c607cd501c2a77cd6
James Bond <james@devbootcamp.com>
2013-11-09 14:49:34 -0800

    incorporate code from starter code in socrates

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,17 @@
 get '/' do
-  # Look in app/views/index.erb
+  @events = Event.all
   erb :index
+end
+
+get '/events/:id/show' do |id|
+  @event = Event.find(id)
+  erb :event_show
+end
+
+get '/events/new' do
+  #TODO IMPLEMENT ME
+end
+
+post '/events/create' do
+  #TODO IMPLEMENT ME
 end
