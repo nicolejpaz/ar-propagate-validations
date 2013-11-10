5c01c4f77f0548df338d9732db96baba67933e5c
Nicole Paz <nicole@Nicoles-MacBook-Pro.local>
2013-11-10 11:52:04 -0800

    finished validations

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -9,9 +9,19 @@ get '/events/:id/show' do |id|
 end
 
 get '/events/new' do
-  #TODO IMPLEMENT ME
+  erb :event_new
 end
 
 post '/events/create' do
-  #TODO IMPLEMENT ME
+  @event_title = params[:title]
+  @event_name = params[:organizer_name]
+  @event_email = params[:organizer_email]
+  @event_date = params[:date]
+  @event = Event.create(title: params[:title], organizer_name: params[:organizer_name], organizer_email: params[:organizer_email], date: params[:date])
+  if @event.errors.messages != {}
+    @errors = @event.errors
+    erb :event_new
+  else
+    redirect '/'
+  end
 end
